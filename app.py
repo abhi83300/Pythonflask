1ab2c7d3c8edf79f0cf44003e8dbf533f04fc1f2
abhi83300 <142506310+abhi83300@users.noreply.github.com>
2023-08-17 23:55:50 +0530

    app.py

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/<nam>')
-def home(nam):
-    return "Hello " + nam
-@app.route('/about')
-def about():
-    return "this is about page"
-
-@app.route('/product')
-def product():
-    return "this is product page"
-
-@app.route('/product/laptop')
-def laptop():
-    return "this is product lap page"
-
-
-
+@app.route('/')
+def home():
+    return "Hello this is home page" 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run()
+
 
